40066f861ea1c7d02d50cd2aab787f9aa82e264a
Jason Pearson <jason.d.pearson@gmail.com>
2013-08-13 20:01:43 -0600

    added necessary depenedencies

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,25 @@ object ApplicationBuild extends Build {
 
   val appName         = "play-cake"
   val appVersion      = "1.0-SNAPSHOT"
-  val appDependencies = Seq()
-  val main = play.Project(appName, appVersion, appDependencies)
+  val appDependencies = Seq(
+    jdbc,
+    anorm,
+    "mysql" % "mysql-connector-java" % "5.1.22",
+    "com.typesafe.play.extras" %% "iteratees-extras" % "1.0.1",
+    "org.specs2" %% "specs2" % "2.1.1",
+    "org.mockito" % "mockito-all" % "1.9.0"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    scalacOptions ++= Seq(
+      "-encoding",
+      "UTF-8",
+      "-deprecation",
+      "-unchecked",
+      "-feature"
+    ),
+    resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+                    "releases"  at "http://oss.sonatype.org/content/repositories/releases")
+  )
 
 }
